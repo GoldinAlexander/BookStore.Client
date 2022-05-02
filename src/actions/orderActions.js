4e2e9d6093b5fab2281e5378e8eacadbd9152e8e
Alex Goldin <goldin.alexandr.m.1960@gmail.com>
2022-05-02 22:45:33 +0300

    Изменил URL в productAction.js, orderAction.js для запросов к BookStore.Server развернутому на Somee.com

--- src/actions/orderActions.js
+++ src/actions/orderActions.js
@@ -1,7 +1,7 @@
 import { CREATE_ORDER, CLEAR_CART, CLEAR_ORDER, FETCH_ORDERS } from "../types";
 
 export const createOrder = (order) => (dispatch) => {
-  fetch("https://localhost:5001/api/Users", {
+  fetch("http://eSaleBookStore.somee.com/api/Users", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -19,7 +19,7 @@ export const clearOrder = () => (dispatch) => {
   dispatch({ type: CLEAR_ORDER });
 };
 export const fetchOrders = () => (dispatch) => {
-  fetch("https://localhost:5001/api/Admin/Index")
+  fetch("http://eSaleBookStore.somee.com/api/Admin/Index")
     .then((res) => res.json())
     .then((data) => {
       dispatch({ type: FETCH_ORDERS, payload: data });
